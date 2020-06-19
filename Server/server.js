a95b092fcb6e7a66e409fa26db0eba4bd7f0fb02
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-19 17:28:37 +0100

    Initial commit: write POST and GET API

---
+++ Server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json("Hello Amazon clone");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Hi there! Your app is listening on port 3000");
+  }
+});
