3ed98f778c4199d16b0a4e1ffaa540b1c1dbeb1c
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-19 20:41:02 +0100

    created user schema and setup environment variable

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(
+  process.env.DATABASE,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("connected to database");
+    }
+  }
+);
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
