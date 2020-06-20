4c87c10d398e6c138364f871a6fd0bfb3dd71441
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-20 11:39:30 +0100

    created Owner API

---
+++ Server/routes/owner.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const Owner = require("../models/owner");
+const { route } = require("./product");
+
+//POST request
+router.post("/owners", async (req, res) => {
+  try {
+    let owner = new Owner();
+    owner.name = req.body.name;
+    owner.about = req.body.about;
+    owner.photo = req.body.photo;
+
+    await owner.save();
+
+    res.json({
+      success: true,
+      message: "Successfully created a new owner",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//GET request
+router.get("/owners", async (req, res) => {
+  try {
+    let owners = await Owner.find();
+
+    res.json({
+      sucess: true,
+      owner: owners,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+module.exports = router;
