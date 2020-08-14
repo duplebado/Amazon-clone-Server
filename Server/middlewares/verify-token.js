28f7bae193ea86556a2c773432621b2884c90057
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-08-14 13:36:25 +0100

    created profile route and tested JWT middleware

--- Server/middlewares/verify-token.js
+++ Server/middlewares/verify-token.js
@@ -1,28 +1,29 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-module.exports = functions(req, res, next) {
-    let token = req.headers['x-access-token'] || req.headers["authorization"]
-    let checkBearer = "Bearer "
+module.exports = function (req, res, next) {
+  let token = req.headers["x-access-token"] || req.headers["authorization"];
+  let checkBearer = "Bearer ";
+
+  if (token) {
     if (token.startsWith(checkBearer)) {
-        token = token.slice(checkBearer.length, token.length)
+      token = token.slice(checkBearer.length, token.length);
     }
 
-    if (token) {
-        jwt.verify(token,process.env.SECRET, (err, decoded) => {
-            if (err) {
-                res.json({
-                    success: false,
-                    message: "Failed to authenticate"
-                })
-            } else {
-                req.decoded = decoded;
-                next()
-            }
-        })
-    } else {
-        req.json({
-            success: false,
-            message: "No token provided"
-        })
-    }
-}
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: "Failed to authenticate",
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "No token provided",
+    });
+  }
+};
